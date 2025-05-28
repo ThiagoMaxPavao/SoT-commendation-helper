df98a43e192ae08f588ee34bd2aa7b70ac3d970e
Thiago Pavao <thipavao44@gmail.com>
2025-05-28 15:25:56 -0300

    swap coin images by text before processing

--- generate_commendations_json.js
+++ generate_commendations_json.js
@@ -7,7 +7,16 @@ rows.forEach(row => {
   if (th_cells.length == 2 && td_cells.length == 2) {
     const commendation = th_cells[1].innerText.trim();
     const rewardsCell = td_cells[1];
-    const rewards = td_cells[1].innerText.trim();
+
+    // Replace images with their alt text - for Doubloons and Gold
+    rewardsCell.querySelectorAll('img[alt]').forEach(img => {
+      if(img.alt != "Doubloons" && img.alt != "Gold") return;
+      
+      const altText = document.createTextNode(img.alt);
+      img.parentNode.replaceChild(altText, img);
+    });
+
+    const rewards = td_cells[1].textContent.trim();
     const links = Array.from(rewardsCell.querySelectorAll('a')).map(a => ({
       text: a.innerText.trim(),
       href: a.href
